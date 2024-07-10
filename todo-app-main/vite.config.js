1fc50deb780802333242977913d3ee01d92b4bca
aadrika <aadrikasrivastava1@gmail.com>
2024-07-10 14:28:38 +0530

    last

--- todo-app-main/vite.config.js
+++ todo-app-main/vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],
   build: {
-    outDir: 'public'
-  }
-})
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html',
+    },
+  },
+  publicDir: 'public',
+  base: './',  // Ensure paths are resolved correctly
+});
